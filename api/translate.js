6838bd17ed81b91b37f0c15bdb4c3cc104f91eeb
unknown <maurice13stu@gmail.com>
2026-07-06 16:23:00 -0700

    Fix ESM to CommonJS export for Vercel compatibility

--- api/translate.js
+++ api/translate.js
@@ -247,7 +247,7 @@ async function callDeepLTranslate(q, target, source) {
   throw err;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Only allow POST requests
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
